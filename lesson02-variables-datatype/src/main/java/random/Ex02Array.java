0e76d28bd6812313a2311e0188186e2bccedf0e1
cuongnnc2610 <cuongnnc2610@gmail.com>
2020-01-15 16:06:06 +0700

    Lesson02 - CODE - 14.01.2020

---
+++ lesson02-variables-datatype/src/main/java/random/Ex02Array.java
@@ -0,0 +1,27 @@
+package random;
+
+import java.util.Random;
+
+public class Ex02Array {
+
+	public static void main(String[] args) {
+		Random rd = new Random();
+		String[] students = {"Lan", "Hoa", "Dao", "Truc"};
+		String first = students[rd.nextInt(students.length)];
+		String second = students[rd.nextInt(students.length)];
+		while (second.equals(first)) {
+			second = students[rd.nextInt(students.length)];
+		}
+		System.out.println(first);
+		System.out.println(second);
+		
+		String test = "" + rd.nextInt(10) + rd.nextInt(10) + rd.nextInt(10);
+		
+		System.out.println("==============================");
+		
+		for (String student: students) {
+			System.out.println(student);
+		}
+	}
+
+}
